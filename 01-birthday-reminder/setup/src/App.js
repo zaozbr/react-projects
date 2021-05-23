a23736af7d461b9523132b942931a8bdb8e595f2
zaozbr <zaozao2@gmail.com>
2021-05-23 18:28:11 -0300

    Birthdary Reminda Zao final

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,24 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data)
+  useState(people.length - 1)
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button className='danger' onClick={() => setPeople([])}>
+          Clear All
+        </button>
+        <button className='goAction' onClick={() => setPeople(data)}>
+          Update page
+        </button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
